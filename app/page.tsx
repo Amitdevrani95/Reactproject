dbee30adaf7b4da769947c908dafe68c77e83ea0
Amit Devrani <amitdevraniad@gmail.com>
2026-01-10 21:58:31 +0530

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import { Metadata } from "next";
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import Brands from "@/components/Brands";
 import HomeClient from "@/components/HomeClient";
 
-export const metadata: Metadata = {
-  title: "SANPEC-EXCELLENCE",
-  description: "This is Home for Startup Nextjs Template",
+const metadata = {
+  title: "SANPEC EXCELLENCE",
+  description: "",
 };
 
 export default function Home() {
-  return (
-    <>
-      <HomeClient />
-      <AboutSectionOne />
-      <Brands />
-    </>
-  );
+  return <HomeClient />;
 }
--- components/HomeClient.tsx
+++ components/HomeClient.tsx
@@ -7,13 +7,18 @@ const PieWheel = dynamic(() => import("./PieWheel"), { ssr: false });
 const Projects = dynamic(() => import("./Projects"), { ssr: false });
 const Blog = dynamic(() => import("./Blog"), { ssr: false });
 
+import AboutSectionOne from "./About/AboutSectionOne";
+import Brands from "./Brands";
+
 export default function HomeClient() {
   return (
     <>
       <HeroSlider />
       <PieWheel />
       <Projects />
+      <AboutSectionOne />
       <Blog />
+      <Brands />
     </>
   );
 }
